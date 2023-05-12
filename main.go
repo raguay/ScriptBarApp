27042d7fbd07b774ba426be15f34b2040547bce7
Richard Guay <raguay@customct.com>
2023-05-12 13:05:13 +0700

    Updating Libraries

--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"embed"
 	"log"
 
-	"github.com/wailsapp/wails/v2/pkg/options/mac"
-
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
+
+//
+// Stuff for wails application
+//
 
 //go:embed frontend/dist
 var assets embed.FS
